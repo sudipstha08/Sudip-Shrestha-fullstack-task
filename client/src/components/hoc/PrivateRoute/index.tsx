735554932026aeb0b8412fd024516d08a1086480
sudipstha08 <sudipstha08@gmail.com>
2024-06-15 14:45:11 +0545

    feat: hocs for handling auth state

---
+++ client/src/components/hoc/PrivateRoute/index.tsx
@@ -0,0 +1,36 @@
+import React, { ComponentType, ReactNode } from 'react'
+import { useNavigate } from 'react-router'
+import { useSnapshot } from 'valtio'
+import { authStore } from '@/store'
+import { SESSION_KEY } from '@/constants'
+import { getItemFromLocalStorage } from '@/utils'
+import { Loader } from '@/components'
+
+const token = getItemFromLocalStorage(SESSION_KEY)
+
+export const PrivateRoute = (AuthComponent: ComponentType) => {
+  function PrivateComponent({ children }: { children: ReactNode }) {
+    const navigate = useNavigate()
+    const { loggedIn, tokenFetching } = useSnapshot(authStore)
+
+    if (!loggedIn && !tokenFetching && !token) {
+      navigate('/login')
+    }
+
+    if (tokenFetching) {
+      return <Loader />
+    }
+
+    return <>{loggedIn && children} </>
+  }
+
+  return class Higher extends React.Component {
+    render() {
+      return (
+        <PrivateComponent>
+          <AuthComponent {...this.props} />
+        </PrivateComponent>
+      )
+    }
+  }
+}
